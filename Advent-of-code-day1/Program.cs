34f86a56ec3b252898cc5d8ae403a13e10793f76
Lovro Dijanović <lovrodijanovic98@gmail.com>
2021-12-02 11:03:31 +0100

    added day1 and day2

---
+++ Advent-of-code-day1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Advent_of_code_d1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            firstPuzzle();
+            secondPuzzle();
+        }
+
+        static void firstPuzzle()
+        {
+            int counter = 0;
+            List<int> numbers = new List<int>();
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\Loc\Desktop\Advent of Code\Advent-of-code-day1\input.txt"))
+            {
+                int number = int.Parse(line);
+                numbers.Add(number);
+            }
+
+            int[] nums = numbers.ToArray();
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+
+        static void secondPuzzle()
+        {
+            int counter = 0;
+            List<int> numbers = new List<int>();
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(@"C:\Users\Loc\Desktop\Advent of Code\Advent-of-code-day1\input.txt"))
+            {
+                int number = int.Parse(line);
+                numbers.Add(number);
+            }
+
+            int[] numbersArray = numbers.ToArray();
+
+            List<int> sums = new List<int>();
+
+            for(int i = 0; i < numbersArray.Length-2; i++)
+            {
+                sums.Add(numbers[i] + numbers[i + 1] + numbers[i + 2]);
+            }
+
+            int[] sumsArray = sums.ToArray();
+
+            for(int i = 1; i < sumsArray.Length; i++)
+            {
+                if(sumsArray[i] > sumsArray[i - 1])
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+
+        }
+    }
+}
---
+++ Advent-of-code-day2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advent_of_code_day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
